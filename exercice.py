00b63ebe2788f49cab93e626c58328446a970c16
SkyLabSeb <77995832+SkyLabSeb@users.noreply.github.com>
2021-03-29 17:38:43 -0400

    update 1

--- exercice.py
+++ exercice.py
@@ -4,6 +4,8 @@
 
 # TODO: Importez vos modules ici
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
@@ -13,14 +15,84 @@ def linear_values() -> np.ndarray:
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+
+    return np.array([(np.sqrt(coord[0]**2 + coord[1]**2), np.arctan2(coord[1], coord[0])) for coord in cartesian_coordinates])
+
+    # coord_polaire = []
+    # for coord in cartesian_coordinates:
+    #     x = coord[0]
+    #     y = coord[1]
+    #     rayon = np.sqrt(x**2 + y**2)
+    #     angle = np.arctan2(y, x)
+    #     coord_polaire.append((rayon, angle))
+    #
+    # return np.array(coord_polaire)
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
-    return 0
+
+    return np.abs(values - number).argmin()
+
+
+def creat_plot():
+    x = np.linspace(start=-1, stop=1, num=250)
+    y = x**2 * np.sin(1 / x**2) + x
+
+    plt.scatter(x, y, label="Ca represente y")
+    plt.xlabel("axe des x")
+    plt.ylabel("axe des y")
+    plt.title("exercice chap 10")
+    plt.xlim((-2, 2))
+    plt.legend()
+    plt.show()
+
+
+def estimation_pi(iteration: int=5000):
+    x_inside_circle = []
+    x_outside_circle = []
+    y_inside_circle = []
+    y_outside_circle = []
+
+    for i in range(iteration):
+        x = np.random.random()
+        y = np.random.random()
+
+        if np.sqrt(x**2 + y**2) <= 1.0:
+            x_inside_circle.append(x)
+            y_inside_circle.append(y)
+        else:
+            x_outside_circle.append(x)
+            y_outside_circle.append(y)
+
+    plt.scatter(x_inside_circle, y_inside_circle, label="inside circle")
+    plt.scatter(x_outside_circle, y_outside_circle, label="outside circle")
+    plt.xlabel("axe des x")
+    plt.ylabel("axe des y")
+    plt.title("estimation valeur de pi")
+    plt.legend()
+    plt.show()
+
+    return len(x_inside_circle) / iteration * 4
+
+    # x = 1 - np.random.random(int(iteration))
+    # y = 1 - np.random.random(int(iteration))
+    # insideX, insideY = x[(x * x + y * y) <= 1], y[(x * x + y * y) <= 1]
+    # outsideX, outsideY = x[(x * x + y * y) > 1], y[(x * x + y * y) > 1]
+    #
+    # fig, ax = plt.subplots(1)
+    # ax.scatter(insideX, insideY, c='y', alpha=0.8)
+    # ax.scatter(outsideX, outsideY, c='g', alpha=0.8)
+    # ax.set_aspect('equal')
+    # fig.show()
+
+    # print(4 * (/ n))
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    print(linear_values())
+    # print(linear_values())
+    # print(coordinate_conversion(cartesian_coordinates=np.array([(15, 30), (-7, 10)])))
+    # print(find_closest_index(np.array([10, 15, 20, 12, 13]), 11.1))
+    # creat_plot()
+    print(estimation_pi(100000))
